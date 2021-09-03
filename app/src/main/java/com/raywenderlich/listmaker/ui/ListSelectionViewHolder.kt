75bc8d3dcc41582cc5c5818385253b37d92ff846
Daryl Cornelius <69173478+dmc1983@users.noreply.github.com>
2021-09-03 10:49:20 -0400

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/listmaker/ui/ListSelectionViewHolder.kt
@@ -0,0 +1,8 @@
+package com.raywenderlich.listmaker.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.listmaker.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder (val binding: ListSelectionViewHolderBinding) :
+RecyclerView.ViewHolder(binding.root){
+}
